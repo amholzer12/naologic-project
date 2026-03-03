e8247c445db0298a0e27bcb2f02396025b3ecb6e
Austin Holzer <amholzer12@gmail.com>
2026-03-03 13:37:53 -0800

    initial commit: Angular project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Timeline } from './timeline/timeline';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Timeline }
+];
